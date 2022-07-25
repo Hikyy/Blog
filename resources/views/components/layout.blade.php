5a5bae0f580d5a29bb19d1254fb1362e9cfb7f6f
Rayane <mabroukr1997@gmail.com>
2022-07-25 14:35:32 +0200

    New

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,15 +28,27 @@
 @include('components._posts-header')
 
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    <x-post-featured-card :post="$posts"/>
 
+    @if ($posts->count())
+    <x-post-featured-card :post="$posts[0]"/>
 
-    {{-- <div class="lg:grid lg:grid-cols-2">
-        <x-postcard/>
-        <x-postcard/>
+    @if ($posts->count()>1)
+    <div class="lg:grid lg:grid-cols-6">
+        @foreach ($posts->skip(1) as $post)
+
+        <x-postcard :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />
+        
+        @endforeach
     </div>
+    @endif
 
-    <div class="lg:grid lg:grid-cols-3">
+    @else
+        <p class="text-center">No posts yet. Please check back later.</p>
+    @endif
+
+
+
+   {{-- <div class="lg:grid lg:grid-cols-3">
         <x-postcard/>
         <x-postcard/>
         <x-postcard/>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -26,7 +26,7 @@
 
 
 
-        <x-layout :posts="$posts[0]" />
+        <x-layout :posts="$posts" />
 
         <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
             <img src="./images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
