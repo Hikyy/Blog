7dd8127f9f9ef41bb25be2d463c4524aec0fb218
Rayane <mabroukr1997@gmail.com>
2022-07-27 14:24:59 +0200

    New

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,14 +10,16 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index',[
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6)->withQueryString(),
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(
+                        request(['search', 'category', 'author'])
+                    )->paginate(6)->withQueryString()
         ]);
     }
 
-    public function show (Post $post)
+    public function show(Post $post)
     {
-        return view('posts.show',[
+        return view('posts.show', [
             'post' => $post
         ]);
     }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,49 +1,60 @@
-{{-- @extends('layout')
+<!doctype html>
 
-@section('top')
-    <h1>Ohayo</h1>
-@endsection
+<title>Laravel From Scratch Blog</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
 
-@section('content')
-    @foreach ($posts as $post)
-        <a href="/posts/{{$post->slug}}">
-            <h1>
-                {!!$post->title!!}
-            </h1>
-        </a>
-
-        <p>
-            By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-        </p>
-
-        <div>
-            {!!$post->body!!}
-        </div>
-    @endforeach
-@endsection --}}
-
-@props(['posts', 'currentCategory'])
+<body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+        <nav class="md:flex md:justify-between md:items-center">
+            <div>
+                <a href="/">
+                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                </a>
+            </div>
 
 
-@include('posts._header')
+            <div class="mt-8 md:mt-0">
+                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
 
-<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    {{ $slot }}
-    @if ($posts->count())
+                <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                    Subscribe for Updates
+                </a>
+            </div>
+        </nav>
 
-    <x-posts-grid :posts="$posts" />
-    {{ $posts->links() }}
-    @else
-        <p class="text-center">No posts yet. Please check back later.</p>
-    @endif
+        <?php $Exist = empty($currentCategory) ? 'Categories' : $currentCategory ?>
 
+        {{ $slot }}
 
+        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
+            <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
 
-   {{-- <div class="lg:grid lg:grid-cols-3">
-        <x-postcard/>
-        <x-postcard/>
-        <x-postcard/>
+            <div class="mt-10">
+                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
 
+                    <form method="POST" action="#" class="lg:flex text-sm">
+                        <div class="lg:py-3 lg:px-5 flex items-center">
+                            <label for="email" class="hidden lg:inline-block">
+                                <img src="/images/mailbox-icon.svg" alt="mailbox letter">
+                            </label>
 
-    </div> --}}
-</main>
+                            <input id="email" type="text" placeholder="Your email address"
+                                   class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                        </div>
+
+                        <button type="submit"
+                                class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
+                        >
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </footer>
+    </section>
+</body>
