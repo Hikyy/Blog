fa71ae24a6ffb38abbe63cd186233f6c6eb642ce
Rayane <mabroukr1997@gmail.com>
2022-08-02 10:01:36 +0200

    New

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -10,5 +11,25 @@ class RegisterController extends Controller
     {
         return view('register.create');
 
+    }
+
+    public function store ()
+    {
+
+        $attributes = request()->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:7|max:255'
+        ]);
+
+        // dd(request()->all());
+        //$attributes['password'] = bcrypt($attributes['password']);
+
+        $user = User::create($attributes);
+
+        auth()->login($user);
+
+        return redirect('/')->with('success', 'Your Account has been created');
     }
 }
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,3 +1,52 @@
 <x-layout>
-    hello
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
+                <h1 class="text-center font-bold text-xl">Register!</h1>
+
+                <form method="POST" action="/register" class="mt-10">
+                    @csrf
+                    <label for="naame">Name</label>
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                    name="name"
+                    id="naame" value="{{ old('name') }}" type="text">
+
+                    @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
+                    <label for="username">Username</label>
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                    name="username"
+                    id="username" value="{{ old('username') }}" type="text">
+
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
+                    <label for="email">Email</label>
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                    name="email"
+                    id="email" value="{{ old('email') }}" type="text">
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
+                    <label for="password">Password</label>
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                    name="password"
+                    id="password" type="password">
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
+                    <button type="submit"
+                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
+                    Sign up
+                    </button>
+                    <x-flash />
+                </form>
+        </main>
+    </section>
 </x-layout>
